97563f2fbf9efc92312c634eb1e43e2bc9dc1fe8
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-16 11:59:38 -0700

    added jeep sales main method

---
+++ src/main/java/com/jed/jeep1/JeepSales.java
@@ -0,0 +1,12 @@
+package com.jed.jeep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepSales {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+  }
+}
