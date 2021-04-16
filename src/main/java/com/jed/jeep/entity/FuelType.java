3ef48473191087e1ffa9e1fb63dcd88a6e5161e2
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-16 11:54:33 -0700

    Added starter code. added jdbc dependacy.

---
+++ src/main/java/com/jed/jeep/entity/FuelType.java
@@ -0,0 +1,5 @@
+package com.jed.jeep.entity;
+
+public enum FuelType {
+  GASOLINE, DIESEL, HYBRID
+}
---
+++ src/main/java/com/jed/jeep/entity/OptionType.java
@@ -0,0 +1,5 @@
+package com.jed.jeep.entity;
+
+public enum OptionType {
+  DOOR, EXTERIOR, INTERIOR, STORAGE, TOP, WHEEL
+}
